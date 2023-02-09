977224c57f840282eb4e1711c76c7de2b0c4ff83
Mowa-Gabriels <mowagabriels@gmail.com>
2023-02-09 02:59:10 +0100

    registration

---
+++ authentication/models.py
@@ -0,0 +1,38 @@
+
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from .managers import UserManager
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField('first name', max_length=30)
+    last_name = models.CharField('last name', max_length=30, blank=True)
+    is_active = models.BooleanField('active', default=True)
+    is_staff     = models.BooleanField('staff', default=True)
+    is_verified    = models.BooleanField('verified', default=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+
+
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    class Meta:
+        verbose_name = ('username')
+        verbose_name_plural =('users')
+
+    def __str__(self):
+        return self.email
+    
+    def token(self):
+        return ''
+
+  
---
+++ authentication/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+
+
+class RegisterSerializer(ModelSerializer):
+    password = serializers.CharField(
+        min_length=6, max_length=70, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = [
+            "email","first_name", "password"
+        ]
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        first_name = attrs.get('first_name', '')
+
+        if not first_name.isalnum():
+            raise serializers.ValidationError("Useranme should only contain alphanumerals")
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+    
+        return user
